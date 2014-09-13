d883840ab7f3f519b81448e0b3532ebe811e7615
Carlos Pastor <carlos.pastor@mailonline.co.uk>
2014-09-13 18:53:15 +0100

    Starting to make the app

--- www/js/monster/MonsterGameCtrl.js
+++ www/js/monster/MonsterGameCtrl.js
@@ -1,7 +1,35 @@
 angular.module('barcodeMonsters')
-    .controller('MonsterGameCtrl', ['$scope', function($scope){
-        $scope.msg = "MonsterGameCtrl";
+    .constant('state', {
+        'normal': {
+            'img': 'img/Monster3/normal.png'
+        },
+        'confused': {
+            'img': 'img/Monster3/confused.png'
+        },
+        'happy': {
+            'img': 'img/Monster3/happy.png'
+        },
+        'sad': {
+            'img': 'img/Monster3/sad.png'
+        },
+        'dead': {
+            'img': 'img/Monster3/dead.png'
+        }
+    })
+    .controller('MonsterGameCtrl', ['$scope', 'state', function($scope, state){
+        var states = ['normal', 'happy', 'normal', 'sad', 'confused', 'dead'];
+        var currentPos = 0;
+        $scope.state = state.happy;
 
+
+        $scope.scanFood = function(){
+            $scope.state = state[states[currentPos]];
+            currentPos++;
+
+            if(currentPos === 5){
+                currentPos = 0;
+            }
+        };
         console.log('MONSTER GAME');
 
 }]);
