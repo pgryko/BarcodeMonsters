77ebc2774b845261d73f28771ac31347a5b2192c
Joseph Finlayson <joseph.finlayson@gmail.com>
2014-09-13 17:32:30 +0100

    session integration

--- www/js/services.js
+++ www/js/services.js
@@ -1,10 +1,34 @@
+'use strict';
 angular.module('starter.services', [])
 
 /**
  * A simple example service that returns some data.
  */
-.factory('ProductsFactory', function($q) {
-    var promise = $q.defer()
+.factory('ProductsFactory', function($q, $http) {
+    var urlRoot = 'https://secure.techfortesco.com/groceryapi/restservice.aspx?';
+    var promise = $q.defer();
+
+    function logFunction(a) {
+        console.log(a);
+        console.log("a");
+
+    }
+
+    function getSessionkey() {
+        console.log("getting")
+        return $http.jsonp(urlRoot + 'command=LOGIN&JSONP=JSON_CALLBACK&email=&password=&developerkey=0ujRTU8FlFnyfad11Ium&applicationkey=417361BD075D0FCFD502')
+            .then(function(prod) {
+                return prod.data.SessionKey;
+            });
+    }
+
+    function getProductData(barcode) {
+        getSessionkey().then(function(prod) {
+        $http.jsonp(urlRoot)        // http://www.techfortesco.com/groceryapi_b1/restservice.aspx?
+        // command=PRODUCTSEARCH&searchtext=chocolate&page=1&se
+
+        });
+    }
 
     function getProductByBarcode(barcode) {
         var object = {
@@ -15,7 +39,7 @@ angular.module('starter.services', [])
                 protein: 34,
             }
         }
-
+        getProductData()
         promise.resolve(object)
         return promise.promise
     }
