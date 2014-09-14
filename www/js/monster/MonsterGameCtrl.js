b7c2a2143f17d8edc0db3b71df015b661b1508dc
Carlos Pastor <carlos.pastor@mailonline.co.uk>
2014-09-14 08:59:40 +0100

    Making the monster eat
    cleaning up index htmls

--- www/js/monster/MonsterGameCtrl.js
+++ www/js/monster/MonsterGameCtrl.js
@@ -1,33 +1,40 @@
 'use strict';
-angular.module('barcodeMonsters').controller('MonsterGameCtrl', ['$scope', 'state', 'Product', 'barcode', 'getState',
-        function($scope, state, Product, barcode, getState) {
-            $scope.state = state.happy;
-            $scope.barcode = 'UNKNOWN';
+angular.module('barcodeMonsters').controller('MonsterGameCtrl', ['$scope', 'state', 'Product', 'barcode', 'getState', '$interval',
+    function ($scope, state, Product, barcode, getState, $interval) {
+        $scope.state = state.happy;
+        $scope.barcode = 'UNKNOWN';
 
-            $scope.scanFood = function() {
-                barcode.scan()
-                    .then(getProductData)
-                    .then(processMonsterState, showError);
-            };
+        $scope.scanFood = function () {
+            barcode.scan()
+                .then(showMonsterEating)
+                .then(getProductData)
+                .then(processMonsterState, showError);
+        };
 
-            //Local functions
-            function getProductData(res) {
-                return Product.get(res.text);
-            }
-
-            function processMonsterState(product){
-                if(!product){
-                    $scope.state = state.confused;
-                    return;
-                }
-
-                $scope.state = getState(product);
-            }
-
-            function showError(error) {
-                $scope.state = state.confused;
-                console.log(Error);
-            }
-
+        //Local functions
+        function showMonsterEating(res) {
+            var EATING_STATES = 4;
+            var count = 0;
+            return $interval(function () {
+                var stateNum = count % EATING_STATES;
+                $scope.state = state['eating' + stateNum];
+                count++;
+            }, 200, 8).then(function () {
+                return res;
+            });
         }
-    ]);
+
+        function getProductData(res) {
+            return Product.get(res.text);
+        }
+
+        function processMonsterState(product) {
+            $scope.state = getState(product);
+        }
+
+        function showError(error) {
+            $scope.state = state.confused;
+            console.log(Error);
+        }
+    }
+]);
--- www/js/monster/MonsterService.js
+++ www/js/monster/MonsterService.js
@@ -15,6 +15,18 @@ angular.module('barcodeMonsterService', [])
         },
         'dead': {
             'img': 'img/Monster3/dead.png'
+        },
+        'eating0': {
+            'img': 'img/Monster3/eating/eating0.png'
+        },
+        'eating1': {
+            'img': 'img/Monster3/eating/eating1.png'
+        },
+        'eating2': {
+            'img': 'img/Monster3/eating/eating2.png'
+        },
+        'eating3': {
+            'img': 'img/Monster3/eating/eating3.png'
         }
     })
 
@@ -25,7 +37,13 @@ angular.module('barcodeMonsterService', [])
         var states = ['normal', 'happy', 'normal', 'sad', 'confused', 'dead'];
         var currentPos = 0;
         return function (product) {
-            var st = state[states[currentPos]];
+            var st;
+
+            if (!product) {
+                return state.confused;
+            }
+
+            st = state[states[currentPos]];
             currentPos++;
 
             if (currentPos === 5) {
