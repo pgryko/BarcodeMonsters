d6b73a51c67656fc8c5943705124463fad680be0
Carlos Pastor <carlos.pastor@mailonline.co.uk>
2014-09-14 08:59:40 +0100

    aAdding messages to the monster

--- www/js/monster/MonsterService.js
+++ www/js/monster/MonsterService.js
@@ -2,19 +2,40 @@
 angular.module('barcodeMonsterService', [])
     .constant('state', {
         'normal': {
-            'img': 'img/Monster3/normal.png'
+            'img': 'img/Monster3/normal.png',
+            'msg': "I'm OK now"
         },
         'confused': {
-            'img': 'img/Monster3/confused.png'
+            'img': 'img/Monster3/confused.png',
+            'msg': "What is this??"
         },
         'happy': {
-            'img': 'img/Monster3/happy.png'
+            'img': 'img/Monster3/happy.png',
+            'msg': "MMMM, Delicious"
         },
         'sad': {
-            'img': 'img/Monster3/sad.png'
+            'img': 'img/Monster3/sad.png',
+            'msg': "I don't like it"
         },
         'dead': {
-            'img': 'img/Monster3/dead.png'
+            'img': 'img/Monster3/dead.png',
+            'msg': "Dead"
+        },
+        'eating0': {
+            'img': 'img/Monster3/eating/eating0.png',
+            'msg': "."
+        },
+        'eating1': {
+            'img': 'img/Monster3/eating/eating1.png',
+            'msg': ".."
+        },
+        'eating2': {
+            'img': 'img/Monster3/eating/eating2.png',
+            'msg': "..."
+        },
+        'eating3': {
+            'img': 'img/Monster3/eating/eating3.png',
+            'msg': "..."
         }
     })
 
@@ -25,7 +46,13 @@ angular.module('barcodeMonsterService', [])
         var states = ['normal', 'happy', 'normal', 'sad', 'confused', 'dead'];
         var currentPos = 0;
         return function (product) {
-            var st = state[states[currentPos]];
+            var st;
+
+            if (!product) {
+                return state.confused;
+            }
+
+            st = state[states[currentPos]];
             currentPos++;
 
             if (currentPos === 5) {
